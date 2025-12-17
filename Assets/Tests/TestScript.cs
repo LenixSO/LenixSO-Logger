ab018a7dd5afb6b82b7fdc15b7bbfa062a744486
Leniton <leniton.carneiro@gdstecnologia.com.br>
2025-12-17 14:27:16 -0300

    Update TestScript.cs

--- Assets/Tests/TestScript.cs
+++ Assets/Tests/TestScript.cs
@@ -1,4 +1,4 @@
-using LenixSO.Logger;
+using System;
 using UnityEngine;
 using Logger = LenixSO.Logger.Logger;
 
@@ -7,7 +7,17 @@ public class TestScript : MonoBehaviour
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     public static void Initialize()
     {
-        Logger.Log("test1", LogFlags.Flag1);
-        Logger.Log("test2", LogFlags.Flag2);
+        new GameObject("Test").AddComponent<TestScript>();
+    }
+
+    private void Start()
+    {
+        //Logger.Log("test1", LogFlags.Flag1);
+        //Logger.Log("test2", LogFlags.Flag2);
+        //Debug.Log("test default");
+        //Debug.unityLogger.logHandler = new TestLogger(Debug.unityLogger.logHandler);
+        //Debug.Log("test handler");
+        Debug.Log("regular log");
+        Logger.Log("logger log"); 
     }
 }
