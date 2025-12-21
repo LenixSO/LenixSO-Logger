d4f9d6843ee209a4e7a89bf451185bb63f923b66
Leniton <lenitoncarneiro@gmail.com>
2025-12-21 09:22:23 -0300

    update test files

--- Assets/Tests/TestScript.cs
+++ Assets/Tests/TestScript.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using LenixSO.Logger;
 using Logger = LenixSO.Logger.Logger;
 
 public class TestScript : MonoBehaviour
@@ -14,9 +15,7 @@ public class TestScript : MonoBehaviour
     {
         //Logger.Log("test1", LogFlags.Flag1);
         //Logger.Log("test2", LogFlags.Flag2);
-        //Debug.Log("test default");
-        //Debug.unityLogger.logHandler = new TestLogger(Debug.unityLogger.logHandler);
-        //Debug.Log("test handler");
+
         Debug.Log("regular log");
         Logger.Log("logger log"); 
     }
